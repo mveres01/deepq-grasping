b93c1398048d38872e1d27aa598479c52bca9088
Matt Veres <mveres03@gmail.com>
2018-10-31 14:41:24 -0400

    adding corrected mcre estimator - WIP

---
+++ cmcre.py
@@ -0,0 +1,127 @@
+import os
+import copy
+import numpy as np
+import torch
+import torch.optim as optim
+
+from base.network import BaseNetwork
+from base.memory import BaseMemory
+from base.optimizer import CEMOptimizer, UniformOptimizer
+
+
+class Memory(BaseMemory):
+
+    def __init__(self, *args, **kwargs):
+        super(Memory, self).__init__(*args, **kwargs)
+
+    def sample(self, batch_size, batch_idx=None):
+        """Samples grasping episodes rather then single timesteps."""
+
+        # Find where each episode in the memory buffer starts
+        starts = np.where(self.timestep == 0)[0]
+
+        # Pick :batch_size: random episodes
+        batch_idx = np.random.choice(len(starts) - 1, batch_size, replace=False)
+
+        # We've already set the value to be discounted
+        indices = [np.arange(starts[i], starts[i+1]) for i in batch_idx]
+        indices = np.hstack(indices)
+
+        return self[indices]
+
+
+class CMCRE:
+
+    def __init__(self, config):
+
+        self.model = BaseNetwork(**config).to(config['device'])
+
+        self.action_size = config['action_size']
+        self.device = config['device']
+        self.bounds = config['bounds']
+
+        self.cem = CEMOptimizer(**config)
+        self.uniform = UniformOptimizer(**config)
+
+        self.optimizer = torch.optim.Adam(self.model.parameters(),
+                                          config['lrate'],
+                                          betas=(0.5, 0.99),
+                                          weight_decay=config['decay'])
+
+    def get_weights(self):
+        return self.model.state_dict()
+
+    def set_weights(self, weights):
+        self.model.load_state_dict(weights)
+
+    def load_checkpoint(self, checkpoint_dir):
+        """Loads a model from a directory containing a checkpoint."""
+
+        if not os.path.exists(checkpoint_dir):
+            raise Exception('No checkpoint directory <%s>' % checkpoint_dir)
+
+        path = os.path.join(checkpoint_dir, 'model.pt')
+        self.model.load_state_dict(torch.load(path, self.device))
+
+    def save_checkpoint(self, checkpoint_dir):
+        """Saves a model to a directory containing a single checkpoint."""
+
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+
+        path = os.path.join(checkpoint_dir, 'model.pt')
+        torch.save(self.model.state_dict(), path)
+
+    @torch.no_grad()
+    def sample_action(self, state, timestep, explore_prob):
+        """Samples an action to perform in the environment."""
+
+        if np.random.random() < explore_prob:
+            return np.random.uniform(*self.bounds, size=(self.action_size,))
+
+        return self.cem(self.model, state, timestep)[0].detach()
+
+    def train(self, memory, gamma, batch_size, **kwargs):
+
+        # Sample data from the memory buffer & put on GPU
+        #s0, act, r, _, _, timestep = memory.sample(batch_size // 8)
+        s0, act, r, _, _, timestep = memory.sample(1)
+
+        s0 = torch.from_numpy(s0).to(self.device)
+        act = torch.from_numpy(act).to(self.device)
+        r = torch.from_numpy(r).to(self.device)
+        t0 = torch.from_numpy(timestep).to(self.device)
+
+        pred = self.model(s0, t0, act).view(-1)
+
+        # V*(s_t) = \max_a Q*(s_t, a)
+        _, qopt = self.uniform(self.model, s0, t0)
+
+        # A* = E_{s,a}[r(s_t, a_t) +  gamma * V*(s_{t+1}) - V*(s_t)]
+        # Q*(s_t, a_t) = 
+        #    E_{s, a}[r(s_t, a_t) + 
+        #    \sum_{t'=t+1}^T gamma^{t'-t}(r(s_t', a_t') - A*(s_t', a_t'))
+        #advantage = r + gamma * qopt[1:] - qopt[:-1]
+
+        out = r.clone()
+
+        discount = 0
+        for i in reversed(range(s0.shape[0] - 1)):
+
+            # Current step + discounted future step we'eve already calculated
+            discount = gamma * (r[i+1] - (pred[i+1] - qopt[i+1]) + discount)
+
+            out[i] = r[i] + discount
+
+        # Note that the reward 'r' has been discounted in memory.load
+        loss = torch.mean((pred - out) ** 2)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), 10.)
+        self.optimizer.step()
+
+        return loss.item()
+
+    def update(self):
+        pass
